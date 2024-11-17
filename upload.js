383f7ddc4dd11294313706cf186035902d797dbc
Seikam <63324984+Seikam@users.noreply.github.com>
2024-11-17 23:54:02 +0100

    upload.js

--- upload.js
+++ upload.js
@@ -7,7 +7,7 @@ const transcriptsDir = path.join(__dirname, 'transcripts');
 // Vérifier que le dossier des transcriptions existe
 if (fs.existsSync(transcriptsDir)) {
     // Ajouter, commiter et pousser les fichiers de transcription vers GitHub
-    exec('git add transcripts/*.json && git commit -m "Ajout de nouvelles transcriptions" && git push origin main', (err, stdout, stderr) => {
+    exec('git add transcripts/*.json && git commit -m "Ajout de nouvelles transcriptions" && git push origin master', (err, stdout, stderr) => {
         if (err) {
             console.error(`Erreur lors du commit et push: ${err.message}`);
             return;
