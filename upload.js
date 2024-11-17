9684f5bfbdf4376d1214cb491039c6d75dee29c1
Seikam <63324984+Seikam@users.noreply.github.com>
2024-11-17 23:39:45 +0100

    Add files via upload

---
+++ upload.js
@@ -0,0 +1,20 @@
+const { exec } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+const transcriptsDir = path.join(__dirname, 'transcripts');
+
+// Vérifier que le dossier des transcriptions existe
+if (fs.existsSync(transcriptsDir)) {
+    // Ajouter, commiter et pousser les fichiers de transcription vers GitHub
+    exec('git add transcripts/*.json && git commit -m "Ajout de nouvelles transcriptions" && git push origin main', (err, stdout, stderr) => {
+        if (err) {
+            console.error(`Erreur lors du commit et push: ${err.message}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+} else {
+    console.error("Le dossier des transcriptions n'existe pas.");
+}
